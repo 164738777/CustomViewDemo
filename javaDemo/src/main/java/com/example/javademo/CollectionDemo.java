69a247c3ecc2f30d34edfd555c30ab6d5d13914d
qiang lb <anworkmail_q@126.com>
2018-11-20 17:56:24 +0800

    ~

--- javaDemo/src/main/java/com/example/javademo/CollectionDemo.java
+++ javaDemo/src/main/java/com/example/javademo/CollectionDemo.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
  * 作者:  lbqiang on 2018/11/18 17:50
  * 邮箱:  anworkmail_q@126.com
  * 作用:
+ *
+ * 1. LinkedList链表: 增删操作比较快, 查询慢(随机访问, RandomAccess).
+ *    如果元素数量少或者要进行访问, 用ArrayList或者数组.
  */
 public class CollectionDemo {
     public static void main(String[] args) {
